2cb0f133529f1d09a8de7ec89bd77eabea0b65a6
Lavanya Paspula <lpaspula@deloitte.com>
2019-01-27 15:26:23 +0530

    project changes!

---
+++ Angular2MVC/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Angular2MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Angular2MVC/app/Components/home.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    template: `
+               <h1>Welcome to Employee Data Management System</h1>
+               <md-card>
+                    <md-card-content>
+                 <p>
+                     In this application, you can add new employee, edit and delete existing employee.
+                    </p>
+                </md-card-content>
+                </md-card>
+               `
+})
+
+export class HomeComponent {
+}
